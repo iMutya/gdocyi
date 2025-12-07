ea4fe929a904ada6a96595a6d6d73d21a6e364be
iMutya <118417733+iMutya@users.noreply.github.com>
2025-12-07 21:56:38 +0800

    feat(editor): add ruler component for improved layout visualization

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,6 +20,7 @@ import { LineHeightExtension } from '@/extensions/line-height'
 import { Color } from '@tiptap/extension-color'
 import { useEditor, EditorContent } from '@tiptap/react'
 import { useEditorStore } from '@/store/use-editor-store'
+import { Ruler } from './ruler'
 
 
 
@@ -28,6 +29,7 @@ export const Editor = () => {
     const { setEditor } = useEditorStore();
 
     const editor = useEditor({ 
+        immediatelyRender: false,
         onCreate({ editor }) {
             setEditor(editor);
         },
@@ -99,6 +101,7 @@ export const Editor = () => {
   })
     return(
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler />
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />
             </div>
---
+++ src/app/documents/[documentId]/ruler.tsx
@@ -0,0 +1,19 @@
+const markers = Array.from({ length: 83 }, (_, i) => i);
+
+export const Ruler = () => {
+    return (
+        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            <div
+                id="ruler-container"
+                className="max-w-[816px] mx-auto w-full h-full relative"
+            >
+                <div className="absolute inset-x-0 bottom-0 h-full">
+                    <div className="relative h-full"></div>
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
