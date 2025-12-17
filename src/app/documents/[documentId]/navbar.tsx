68aa875ef0b34ba09a1ad01073efbb575f6f883d
iMutya <118417733+iMutya@users.noreply.github.com>
2025-12-17 22:24:47 +0800

    feat(navbar): integrate table insertion functionality and enhance menu item actions

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -19,8 +19,21 @@ import {
 import { DocumentInput } from "./document-input";
 import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlusIcon, FileTextIcon, GlobeIcon, ItalicIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, StrikethroughIcon, TextIcon, TrashIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 import { BsFilePdf } from "react-icons/bs";
+import { useEditorStore } from "@/store/use-editor-store";
 
 export const Navbar = () => {
+    const { editor } = useEditorStore();
+
+    const insertTable = ({rows, cols}: {rows: number, cols: number}) => {
+        editor
+            ?.chain()
+            .focus()  
+            .insertTable({rows, cols, withHeaderRow:false})
+            .run()
+    };
+
+    const onDownload =()
+
     return(
         <nav className="flex items-center justify-between">
             <div className="flex gap-2 items-center">
@@ -85,11 +98,11 @@ export const Navbar = () => {
                                     Edit
                                 </MenubarTrigger>
                                 <MenubarContent>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={() => editor?.chain().focus().undo().run()}>
                                         <Undo2Icon className="size-4 mr-2" />
                                         Undo <MenubarShortcut>Ctrl+Z</MenubarShortcut>
                                     </MenubarItem>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={() => editor?.chain().focus().redo().run()}>
                                         <Redo2Icon className="size-4 mr-2" />
                                         Redo <MenubarShortcut>Ctrl+Y</MenubarShortcut>
                                     </MenubarItem>
@@ -103,16 +116,16 @@ export const Navbar = () => {
                                     <MenubarSub>
                                         <MenubarSubTrigger>Table</MenubarSubTrigger>
                                         <MenubarSubContent>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => insertTable({ rows:1 , cols: 1 })}>
                                                 1 x 1
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => insertTable({ rows:2 , cols: 2 })}>
                                                 2 x 2
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => insertTable({ rows:3 , cols: 3 })}>
                                                 3 x 3
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => insertTable({ rows:4 , cols: 4 })}>
                                                 4 x 4
                                             </MenubarItem>
                                         </MenubarSubContent>
@@ -130,25 +143,25 @@ export const Navbar = () => {
                                             Text
                                         </MenubarSubTrigger>
                                         <MenubarSubContent>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => editor?.chain().focus().toggleBold().run()}>
                                                 <BoldIcon className="size-4 mr-2" />
                                                 Bold <MenubarShortcut>Ctrl+B</MenubarShortcut>
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => editor?.chain().focus().toggleItalic().run()}>
                                                 <ItalicIcon className="size-4 mr-2" />
                                                 Italic <MenubarShortcut>Ctrl+I</MenubarShortcut>
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => editor?.chain().focus().toggleUnderline().run()}>
                                                 <UnderlineIcon className="size-4 mr-2" />
                                                 Underline <MenubarShortcut>Ctrl+U</MenubarShortcut>
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => editor?.chain().focus().toggleStrike().run()}>
                                                 <StrikethroughIcon className="size-4 mr-2" />
-                                                <span>Strikethrough &nbsp;&nbsp;</span> <MenubarShortcut>Alt+Shift+5</MenubarShortcut>
+                                                <span>Strikethrough &nbsp;&nbsp;</span> <MenubarShortcut>Ctrl+Shift+S</MenubarShortcut>
                                             </MenubarItem>
                                         </MenubarSubContent>
                                     </MenubarSub>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={() => editor?.chain().focus().unsetAllMarks().run()}>
                                         <RemoveFormattingIcon className="size-4 mr-2" />
                                         Clear Formatting 
                                     </MenubarItem>
