b5b430fddc28262084234355854b19e2d6e73cde
iMutya <118417733+iMutya@users.noreply.github.com>
2025-12-28 23:36:58 +0800

    feat: enhance document editing experience with real-time updates and improved UI components

---
+++ src/app/documents/[documentId]/loading.tsx
@@ -0,0 +1,8 @@
+import { FullScreenLoader } from "@/components/fullscreen-loader"
+
+const LoadingPage = () => {
+    return <FullScreenLoader label="Document Loading..." />;
+}
+
+
+export default LoadingPage;
--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,11 +1,21 @@
 import { useRef, useState } from "react";
 import { FaCaretDown } from "react-icons/fa"
+import { useStorage, useMutation } from "@liveblocks/react";
 
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 export const Ruler = () => {
-    const [leftMargin, setLeftMargin] = useState(56);
-    const [rightMargin, setRightMargin] = useState(56);
+    const leftMargin = useStorage((root) => root.leftMargin) ?? 56;
+    const setLeftMargin = useMutation(({storage}, position:number) => {
+        storage.set("leftMargin", position)
+    },[]);
+
+    const rightMargin = useStorage((root) => root.rightMargin) ?? 56;
+    const setRightMargin = useMutation(({storage}, position:number) => {
+        storage.set("rightMargin", position)
+    },[]);
+    
+
 
     const [isDraggingLeft, setIsDraggingLeft] = useState(false);
     const [isDraggingRight, setIsDraggingRight] = useState(false);
@@ -33,7 +43,7 @@ export const Ruler = () => {
                 if (isDraggingLeft) {
                     const maxLeftPosition = PAGE_WIDTH - rightMargin - MINIMUM_SPACE;
                     const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
-                    setLeftMargin(newLeftPosition); // make this collaborative
+                    setLeftMargin(newLeftPosition); 
                 }else if (isDraggingRight){
                     const maxRightPosition = PAGE_WIDTH - (leftMargin + MINIMUM_SPACE);
                     const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
@@ -143,7 +153,7 @@ const Marker = ({
             style={{
                 height: "100vh",
                 width: "1px",
-                transform: "scaleX(0.5",
+                transform: "scaleX(0.5)",
                 backgroundColor: "#3b72f6",
                 display: isDragging ? "block" : "none",
 
